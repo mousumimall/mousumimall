53223c81909bc8e30d9f63dfccd6c9983c9af1b4
mousumimall <88453691+mousumimall@users.noreply.github.com>
2021-08-04 23:38:58 +0530

    Create .js

---
+++ .js
@@ -0,0 +1,25 @@
+
+<script>
+
+// Here a date has been assigned according 
+
+// to universal time while creating Date object
+
+var dateobj = 
+
+new Date('October 15, 1996 05:35:32:77 GMT+11:00');
+
+
+// millisecond from above date object is
+
+// being extracted using getUTCMilliseconds().
+
+var B = dateobj.getUTCMilliseconds();
+
+
+// Printing millisecond according 
+
+// to universal time
+
+document.write(B);
+</script>
